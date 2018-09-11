40c69b5f5ec6309fa86c47ea104c10c8e7e2ee5e
Abdellatif SAHLALI user <asahlali@MacBook-Pro-de-mac.local>
2018-09-11 22:02:09 +0100

    Maj

--- src/test/java/fr/auro/workitem/BackEndWorkItemDashBoardApplicationTests.java
+++ src/test/java/fr/auro/workitem/BackEndWorkItemDashBoardApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServeurGestionStockProduitsApplicationTests {
+public class BackEndWorkItemDashBoardApplicationTests {
 
 	@Test
 	public void contextLoads() {
